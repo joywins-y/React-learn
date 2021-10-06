a9be2676091f84e44db26fc434c24383a9f7807a
Fiat_lux <fiat_lux@Fiat-luxdeMacBook-Air.local>
2021-10-06 12:35:09 +0800

    【demo】图片切换

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import img1 from "./assets/0.png";
+import img2 from "./assets/1.jpg";
+import img3 from "./assets/2.jpeg";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 保存图片路径的数组
+const imgs = [img1, img2, img3];
+// 显示的图片索引
+let index = 1;
+let timer = null;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+
+/**
+ * 根据index的值，显示某张照片
+ */
+function render() {
+  ReactDOM.render(<img alt="111" src={imgs[index]} />, container);
+}
+
+/**
+ * 启动计时器，每隔一段时间，切换图片
+ */
+function start() {
+  stop();
+  timer = setInterval(() => {
+    // 改变index的值
+    index = (index + 1) % 3;
+    render();
+  }, 1000);
+}
+
+/**
+ * 停止计时器
+ */
+function stop() {
+  clearInterval(timer);
+}
+
+container.onmouseenter = function () {
+  stop();
+};
+
+container.onmouseleave = function () {
+  start();
+};
+
+render();
+start();
